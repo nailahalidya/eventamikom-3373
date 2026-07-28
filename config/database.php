978d5af24267292288610b9cd741472052ef2009
nailahalidya <nailah.alidya0907@students.amikom.ac.id>
2026-07-28 21:09:26 +0700

    fix: set PDO non-persistent and timeout options for database connection

--- config/database.php
+++ config/database.php
@@ -61,6 +61,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_PERSISTENT => false,
+                PDO::ATTR_TIMEOUT => 5,
             ]) : [],
         ],
 
